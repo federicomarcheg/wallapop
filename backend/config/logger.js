342a8803de7aff75a3a0950284adff540d83fc39
federicomarcheg <84737604+federicomarcheg@users.noreply.github.com>
2025-01-16 14:10:14 -0300

    Update logger.js

--- backend/config/logger.js
+++ backend/config/logger.js
@@ -1,6 +1,24 @@
 const pino = require('pino');
+const { createLogger, format, transports } = require('winston');
+
+
+
 const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
+    format: format.combine(
+        format.timestamp(),
+        format.printf(({ timestamp, level, message}) => {
+            return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+        })
+    ),
+
+    transports: [
+        new transports.Console(),
+        new transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new transports.File({ filename: 'combined.log' }),
+    ],
+   
+   
     transport: {
         target: 'pino-pretty',
     },
