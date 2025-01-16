34446e3f928b8e316054f1a124562524d6379cfd
federicomarcheg <84737604+federicomarcheg@users.noreply.github.com>
2025-01-16 14:34:34 -0300

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const { registerUser, loginUser } = require(('../controllers/usercontroller'));
-
 const router = express.Router();
+const joi = require('joi');
+const validate = rquire('../middleware/validate');
+const { createUser } = require('../controllers/usercotroller');
+const apiLimiter = require('../config/rateLimiter');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.use('/register', apiLimiter);
+router.post('/register', validate(userSchema), createUser);
+
+
+module.exports = router;
+
+
+const userSchema = join.object({
+    name: joi.string().min(3).required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(6).required(),
+    confirmPassword: joi.ref('password'),
+});
+
+router.post('/register', validate(userSchema), createUser);
 
 module.exports = router;
