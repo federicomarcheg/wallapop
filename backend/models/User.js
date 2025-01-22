6c557e08e8c3c15868af92ab19bd20c2953f07c9
federicomarcheg <84737604+federicomarcheg@users.noreply.github.com>
2025-01-22 13:02:50 -0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
     name:{ Type: String, required: true},
@@ -6,5 +7,22 @@ const UserSchema = new mongoose.Schema({
     password:{ type: String, required: true},
     createdAt:{ type: Date, default: Date.now},
 });
+
+
+
+
+UserSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+})
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    isOnline: { type: Boolean, default: false },
+});
+
+
 
 module.exports = mongoose.model('User', UserSchema);
