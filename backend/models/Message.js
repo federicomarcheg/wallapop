8b25a016a643c1bfcdc673cee3789f27fc6205c2
federicomarcheg <84737604+federicomarcheg@users.noreply.github.com>
2025-01-22 12:59:47 -0300

    Add files via upload

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -5,8 +5,15 @@ const MessageSchema = new mongoose.Schema(
     sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     content: { type: String, required: true },
+    receiverId: mongoose.Schema.Types.ObjectId,
+    text: String,
+    isRead: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now },
   },
   { timestamps: true } 
 );
+
+
+
 
 module.exports = mongoose.model('Message', MessageSchema);
