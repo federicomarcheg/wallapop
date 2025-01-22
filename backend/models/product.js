2015cbd51a35ee4ab249c0e0b13e85a871d6e5ff
federicomarcheg <84737604+federicomarcheg@users.noreply.github.com>
2025-01-22 13:06:03 -0300

    Update product.js

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const Product = require('../models/product');
+
 
 const ProductSchema = new mongoose.Schema({
     title: { type: String, required: true },
@@ -8,5 +10,19 @@ const ProductSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     createdAt: { type: Date, default: Date.now },
 });
+
+
+
+exports.getProductsByCategory = async ( req, res) => {
+    try {
+        const { category } = req.params;
+        const products = await Product.find({ category });
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener productos por categoria' });
+    }
+};
+
+
 
 module.exports = mongoose.model('Product', ProductSchema);
