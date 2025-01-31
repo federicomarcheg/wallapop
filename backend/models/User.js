0a77d2f74fb43f558a87ce0e42acab0ee59a9677
federicomarcheg <84737604+federicomarcheg@users.noreply.github.com>
2025-01-31 13:13:56 -0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const { encrypt, decrypt } = require('../utils/encription');
+
+
+
 
 const UserSchema = new mongoose.Schema({
     name:{ Type: String, required: true},
@@ -18,10 +22,28 @@ UserSchema.pre('save', async function(next) {
     next();
 })
 
+const salt = await bcrypt.genSalt(10);
+this.password = await bcrypt.hash(this.password, salt);
+next();
+
+
 const userSchema = new mongoose.Schema({
     username: String,
     isOnline: { type: Boolean, default: false },
 });
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+
+
+userSchema.pre('save', function (next) {
+    if (this.isModified('sensitiveData')) {
+        this.sensitiveData = encrypt(this.sensitiveData);
+    }
+})
+
 
 
 
